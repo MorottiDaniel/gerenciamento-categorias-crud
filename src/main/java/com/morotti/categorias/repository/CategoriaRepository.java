10b252a5595eac2273363d52667d51ef59ad406b
Daniel Morotti Vieira <morottivieirad@gmail.com>
2026-03-23 20:28:21 -0300

    Repository

---
+++ src/main/java/com/morotti/categorias/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.morotti.categorias.repository;
+
+import com.morotti.categorias.entity.CategoriasEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriasEntity, Long> {
+}
